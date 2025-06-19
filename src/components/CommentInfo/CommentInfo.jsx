7617d9e2bdaa52d27b7d3196eff63696b85392f9
Taras <luchakt2@gmail.com>
2025-06-19 18:51:31 +0300

    Replaced {comment.name} with {comment.email}

--- src/components/CommentInfo/CommentInfo.jsx
+++ src/components/CommentInfo/CommentInfo.jsx
@@ -6,7 +6,7 @@ export const CommentInfo = ({ comment }) => (
       {' by '}
 
       <a className="CommentInfo__email" href={`mailto:${comment.email}`}>
-        {comment.name}
+        {comment.email}
       </a>
     </div>
 
